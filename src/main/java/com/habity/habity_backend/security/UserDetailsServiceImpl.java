8de304b1a1ece3ce038f238bd4e35206314cabbe
karlachunga <u20221a396@upc.edu.pe>
2025-07-05 00:46:43 -0500

    cambio user impl

--- src/main/java/com/habity/habity_backend/security/UserDetailsServiceImpl.java
+++ src/main/java/com/habity/habity_backend/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.habity.habity_backend.security;
 
-import com.habity.habity_backend.entity.Usuario;
 import com.habity.habity_backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,14 +9,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Usuario usuario = usuarioRepository.findByEmail(email)
+        return usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
-        return new UserDetailsImpl(usuario);
     }
+
 }
